1b76421c0f2094f1f8b3e156fcffaa3ed2c4cd33
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-12-15 18:10:07 +0100

    Create CreateAccountPage

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Signin() {
     const [username, setUsername] = useState("");
@@ -11,9 +12,10 @@ export default function Signin() {
             return setError("Glöm inte att fyla Anvöndernamn och Lösenord")
         }
         try {
-            const response = await axios.post("http:/localhost:8080/user/login"
+            const response = await axios.post("http://localhost:8080/user/login"
                 , { username, password });
-                console.log(response);
+                localStorage.setItem("user",JSON.stringify(response))
+                window.location.href="/";
         }
         catch{
             setError("Inloggning misslyckas. Kontrollera dina uppgifter.");
@@ -22,17 +24,24 @@ export default function Signin() {
     
     }
     return (
-        <div>
+        <LoginContainer>
+            {error && <h3>{error}</h3>}
             <h1>Login Sidan</h1>
             <label> Användarenamn</label>
-            <input type="text" value={username}
+            <input type="Username" value={username}
                 onChange={(event) => { setUsername(event.target.value) }} />
             <label> Lösenord</label>
-            <input type="text" value={password}
+            <input type="Password" value={password}
                 onChange={(event) => { setPassword(event.target.value) }} />
+                <br/>
             <button onClick={handelLogin}>Logga in</button>
             <p>Eller skapa ett konto</p>
-            <Link to="/createaccount" >Skappa konto</Link>
-        </div>
+            <button><Link to="/createaccount" >Skappa konto</Link>
+            </button>
+        </LoginContainer>
     );
 }
+const LoginContainer= styled.div`
+display: grid;
+justify-content: center;
+`
