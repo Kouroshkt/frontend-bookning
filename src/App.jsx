e37d764c73b805473071e9deb7b5b4af85aa2123
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2025-05-14 09:32:21 +0200

    Create About Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import RootLayout from "./Rootlayout/RootLayout";
 import { CarInfo } from "./pages/CarInfo";
 import CreateAccount from "./pages/CreateAccount";
 import {ConfirmationPage} from "./pages/ConfirmationPage";
+import { About } from "./pages/About";
 
 export default function App() {
   const router = createBrowserRouter(
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="confirmation" element={<ConfirmationPage />} />
         <Route path="signin" element={<Signin />} />
         <Route path="carinfo/:carId/:startDate/:endDate/:rentalDays" element={<CarInfo />} />
+        <Route path="about" element={<About/>} />
         <Route path="createaccount" element={<CreateAccount />} />
       </Route>
     )
